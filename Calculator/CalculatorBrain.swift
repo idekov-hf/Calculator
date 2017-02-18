1d7d7cff8e753bb90e457b746ae4ada3546b187b
ivdekov <ivdekov@gmail.com>
2017-02-18 16:58:30 -0600

    Finish CalculatorBrain struct; finish MVC implementation of app

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,89 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Iavor Dekov on 2/18/17.
+//  Copyright © 2017 Iavor Dekov. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    // Make optional because when you create CalculatorBrain, there is no accumulated result.
+    // It starts off as nil. Not-set state.
+    private var accumulator: Double?
+    
+    // Embedded type. Full name is CalculatorBrain.Operation
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π" : Operation.constant(Double.pi),
+        "e" : Operation.constant(M_E),
+        "√" : Operation.unaryOperation(sqrt),
+        "cos" : Operation.unaryOperation(cos),
+        "±" : Operation.unaryOperation({ -$0 }),
+        "×" : Operation.binaryOperation({ $0 * $1 }),
+        "÷" : Operation.binaryOperation({ $0 / $1 }),
+        "+" : Operation.binaryOperation({ $0 + $1 }),
+        "−" : Operation.binaryOperation({ $0 - $1 }),
+        "=" : Operation.equals
+    ]
+    
+    mutating func performOperation(_ symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case .equals:
+                performPendingBinaryOperation()
+            }
+        }
+    }
+    
+    private mutating func performPendingBinaryOperation() {
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+            pendingBinaryOperation = nil
+        }
+        // ? -> it will unwrap the optional but if it is not set, it will ignore this line
+        // pbo?.perform(with: accumulator)
+    }
+    
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+    
+    // Read-only property (only has set and not get)
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+    
+}
